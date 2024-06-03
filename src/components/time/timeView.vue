ba8d03f560a1224d13fa550d4fa84468ad803a57
jkjh8 <jkjh82@naver.com>
2024-06-03 16:52:11 +0900

    first commit

---
+++ src/components/time/timeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const currentDateTime = ref("");
+
+const updateDateTime = () => {
+  const now = new Date();
+  const date = now.toLocaleDateString();
+  const time = now.toLocaleTimeString();
+  const day = now.toLocaleDateString(undefined, { weekday: "long" });
+  currentDateTime.value = `${date} ${day} ${time}`;
+};
+
+onMounted(() => {
+  updateDateTime();
+  setInterval(updateDateTime, 1000);
+});
+</script>
+
+<template>
+  <div>
+    {{ currentDateTime }}
+  </div>
+</template>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<script setup>
+import TimeView from "components/time/timeView.vue";
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="bg-white text-black">
+      <q-toolbar class="justify-between">
+        <div class="row no-wrap justify-start items-center q-gutter-x-sm">
+          <q-icon name="img:LogoMain.png" size="md" />
+          <div class="toolbar-font">스케줄 컨트롤러</div>
+        </div>
+        <TimeView />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style scoped>
+.toolbar-font {
+  font-size: 20px;
+  font-weight: 600;
+  margin-left: 10px;
+  font-family: "NotoSansKR";
+  color: #333;
+}
+</style>
